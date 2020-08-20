908bb4fb8ba2ec546ccbb1e4d4ce49c6304da221
honda0924 <honda0924@hotmail.co.jp>
2020-08-20 10:35:44 +0900

    unit_test

--- nodecli/main.js
+++ nodecli/main.js
@@ -1,6 +1,8 @@
 const program = require("commander");
 const fs = require("fs");
-const marked = require("marked");
+const md2html = require("./md2html");
+
+program.option("--gfm","GFMを有効にする");
 program.parse(process.argv);
 const filePath = program.args[0];
 
@@ -15,8 +17,6 @@ fs.readFile(filePath,{encoding: "utf8"},(err,file) => {
     process.exit(1);
     return;
   }
-  const html = marked(file,{
-    gfm: cliOptions.gfm,
-  });
+  const html = md2html(file,cliOptions);
   console.log(html);
 });
